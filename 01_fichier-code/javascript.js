eeebc2d772990e6d73b390e0c98a42d545ff5c73
vince1887 <vincentdesjardins1887@gmail.com>
2022-09-28 18:49:10 -0400

    Update javascript.js

--- 01_fichier-code/javascript.js
+++ 01_fichier-code/javascript.js
@@ -1,5 +1,7 @@
-let michealEnterOffice = {
-  subtitle: "Bon matin!",
-  text: "Micheal apperçoit Jim! va t-il le saluer ou il l'ignore?",
-  img: "imgChap1.png",
+let chapterObj = {
+  michealEnterOffice: (michealEnterOffice = {
+    subtitle: "Bon matin!",
+    text: "Micheal apperçoit Jim! va t-il le saluer ou il l'ignore?",
+    img: "imgChap1.png",
+  }),
 };
