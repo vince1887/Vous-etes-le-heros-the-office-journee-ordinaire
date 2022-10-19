2605eb96c6f5e2277b2978b65a4e58acafecbb17
vince1887 <vincentdesjardins1887@gmail.com>
2022-10-19 19:17:46 -0400

    Update javascript.js

--- 01_fichier-code/javascript.js
+++ 01_fichier-code/javascript.js
@@ -3,85 +3,85 @@ let chapterObj = {
     subtitle: "Bon matin!",
     text: "Micheal apperçoit Jim! va t-il le saluer ou il l'ignore?",
     img: "imgChap1.png",
-    options: (optionMichealEnterOffice = [
-      { text: "Il le salut", action: goToChapter(activationTasse) },
-      { text: "Il l'ignore", action: goToChapter(michealEnterDesk) },
-    ]),
+    options: [
+      { text: "Il le salut", action: goToChapter("activationTasse") },
+      { text: "Il l'ignore", action: goToChapter("michealEnterDesk") },
+    ],
   }),
   activationTasse: (activationTasse = {
     subtitle: "Hey Jim bon matin!",
     text: "salut Micheal voici cette magnifique tasse!!",
     img: "imgTasse.png",
-    option: (optionTasse = {
+    option: {
       text: "Merci Jim!",
-      action: goToChapter(michealEnterDesk),
-    }),
+      action: goToChapter("michealEnterDesk"),
+    },
   }),
   michealEnterDesk: (michealEnterDesk = {
     subtitle: "Bon je dois me mettre au travail maintenant.",
     text: "J'entend un bruit?? ohhh c'est Dwight qui cogne à ma porte.",
     img: "imgdesk.png",
-    options: (optionMichealEnterdesk = [
-      { text: "Il lui ouvre la porte", action: goToChapter(dwightEnterDesk) },
-      { text: "Il l'ignore", action: goToChapter(michealFallAslepp) },
-    ]),
+    options: [
+      { text: "Il lui ouvre la porte", action: goToChapter("dwightEnterDesk") },
+      { text: "Il l'ignore", action: goToChapter("michealFallAslepp") },
+    ],
   }),
   michealFallAslepp: (michealFallAslepp = {
     subtitle: "Tu t'es endormie!",
     text: "Tu n'as pas pris ton café, donc tu t'es endormie.",
     img: "imgasleep.png",
-    options: (optionmichealFallAslepp = [
+    options: [
       {
         text: "Recommence ta journée",
-        action: goToChapter(michealEnterOffice),
+        action: goToChapter("michealEnterOffice"),
       },
-    ]),
+    ],
   }),
   dwightEnterDesk: (dwightEnterDesk = {
     subtitle: "Dwight entre dans ton bureau",
     text: "Bon matin Micheal! Si tu veux on peut aller sur ma ferme de betrave aujourd'hui.",
     img: "imgferme.png",
-    options: (optiondwightEnterDesk = [
-      { text: "Non je ne peut pas", action: goToChapter(michealExcuseCafe) },
-      { text: "Bien sur!", action: goToChapter(farmDay) },
-    ]),
+    options: [
+      { text: "Non je ne peut pas", action: goToChapter("michealExcuseCafe") },
+      { text: "Bien sur!", action: goToChapter("farmDay") },
+    ],
   }),
   farmDay: (farmDay = {
     subtitle: "Tu passe la journée a la ferme de Dwight",
     text: "Tu n'as pas accomplie tes tâches de la journée",
     img: "imgferme.png",
-    options: (optiondfarmDay = [
+    options: [
       {
         text: "Tu dois reccomencer ta journée",
-        action: goToChapter(michealEnterOffice),
+        action: goToChapter("michealEnterOffice"),
       },
-    ]),
+    ],
   }),
   michealExcuseCafe: (michealExcuseCafe = {
     subtitle:
       "Maintenant que tu as tasser Dwight de ton chemin, tu veux te faire une tasse de café.",
     text: "Attention! as-tu saluer Jim se matin?? Car si oui tu vas pouvoir te fair un café, si non tu ne pourras pas ",
     img: "imgMachinecafe.png",
-    options: (optionmichealCafe = [
-      { text: "oui", action: goToChapter(michealFaitCafe) },
-      { text: "non", action: goToChapter(michealPasCafe) },
-    ]),
+    options: [
+      { text: "oui", action: goToChapter("michealFaitCafe") },
+      { text: "non", action: goToChapter("michealPasCafe") },
+    ],
   }),
   michealFaitCafe: (michealFaitCafe = {
     subtitle: "Tu te fais un café",
     text: "Grace à cette tasse de café tu vas pouvoir accomplir toutes tes tâches de la journée!!",
     img: "imgVictoire.png",
-    options: (optionmichealCafe = [
-      { text: "Tu as réussi!!", action: goToChapter(michealEnterOffice) },
-    ]),
+    options: [
+      { text: "Tu as réussi!!", action: goToChapter("michealEnterOffice") },
+    ],
   }),
   michealPasCafe: (michealPasCafe = {
     subtitle: "Tu as oublier ta tasse favorite à la maison.",
     text: "Si tu avais salué Jim il t'aurai remis une tasse en guise de cadeau. Meilleur chance la prochiane fois.",
     img: "imgdefaite.png",
-    options: (optionmichealCafe = [
-      { text: "reccomencer", action: goToChapter(michealEnterOffice) },
-    ]),
+    options: [
+      { text: "reccomencer", action: goToChapter("michealEnterOffice") },
+    ],
   }),
 };
 
@@ -97,6 +97,5 @@ function goToChapter(chapterName) {
   subtitleHtml.innerText = subtitleText;
 
   //point 4
-  let optionArr = chapterName.option;
-  for
+  let optionArr = chapterName.options;
 }
